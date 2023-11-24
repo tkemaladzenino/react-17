4f62bf1deb691e6c661b11099e1e2a70e67c6432
tkemaladzenino <tkemaladzenino71@gmail.com>
2023-11-23 23:36:46 -0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 
 // index.jsx
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Authorization from './Authorization';
